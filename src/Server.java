0086f8b02021fadbdd3b8d645363424f2e1722d9
Homero Oliveira <homero.santos.001@acad.pucrs.br>
2018-10-31 22:30:55 -0300

    fix OK

--- src/Server.java
+++ src/Server.java
@@ -13,10 +13,10 @@ public class Server {
 	public static final String ERRO = "erro";
 	private final Configuration configuration;
 	private final List<Message> messages = Collections.synchronizedList(new ArrayList<>());
+	private Message errorMessage;
 
 	public Server(Configuration configuration) {
 		this.configuration = configuration;
-		sendMessage();
 
 		new Thread(this::lintenClients)
 				.start();
@@ -28,6 +28,7 @@ public class Server {
 				final Message message = Message.from(messageString);
 				if (messages.size() < 10) {
 					messages.add(message);
+					sendMessage();
 				} else {
 					System.out.println("Messagem perdida " + messageString);
 				}
@@ -57,39 +58,53 @@ public class Server {
 				} else {
 					final Message message = Message.from(receivedMessage);
 					if (message == null) {
+						System.out.println("Mensagem invalida " + receivedMessage);
 						continue;
 					}
-					System.out.println(message.toString());
 
+
+					// A mensagem é para mim
 					if (message.getNicknameDestination().equals(configuration.getNickname())) {
 						printMessage(message);
-						message.setErrorControl("ok");
+						message.setErrorControl(OK);
+						// TODO: Fazer o probalidade de ocorrer um erro
 						sendMessageToClient(message.toString());
-					} else if (message.getNicknameDestination().equals(TODOS)
+					}
+					// A mensagem é para todos e não fui eu que enviei e codigo é naocopiado
+					else if (message.getNicknameDestination().equals(TODOS)
 							&& !message.getNicknameSource().equals(configuration.getNickname())
 							&& message.getErrorControl().equals(NAO_COPIADO)) {
 						System.out.println("A mensagem voltou para mim");
 						printMessage(message);
 						sendMessageToClient(message.toString());
-					} else if(!message.getNicknameDestination().equals(configuration.getNickname())) {
-						System.out.println("Recebi não para mim");
-						System.out.println(message.toString());
+					}
+					// Mensagem não é para mim e o origem não sou eu
+					else if(!message.getNicknameDestination().equals(configuration.getNickname())
+							&& !message.getNicknameSource().equals(configuration.getNickname())) {
+						System.out.println(configuration.getNickname()
+								+ " Recebi não é para mim"
+								+ message.toString());
 						sendMessageToClient(message.toString());
-					} else if (message.getErrorControl().equals(OK)
-							|| message.getErrorControl().equals(NAO_COPIADO)) {
-						System.out.println("Foi entregue");
-						sendToken();
-					} else if (message.getErrorControl().equals(ERRO)){
-						if (message.getRetryCount() > 0) {
-							System.out.println("Não foi possivel enviar a mensagem " + message.toString());
-						} else {
-							System.out.println("Não foi possivel enviar a mensagem, voltou para fila. "
-									+ message.toString());
-							message.setRetryCount(message.getRetryCount() + 1);
-							message.setErrorControl(NAO_COPIADO);
-							messages.add(0, message);
+					}
+					// Fui eu que enviei a mensagem e estou recendo de volta
+					else if(message.getNicknameSource().equals(configuration.getNickname())) {
+						System.out.println("é para mim " + message.toString());
+						if (message.getErrorControl().equals(OK)
+								|| message.getErrorControl().equals(NAO_COPIADO)) {
+							System.out.println("Foi entregue " + message.toString());
+							sendToken();
+						} else if (message.getErrorControl().equals(ERRO)){
+							 if(errorMessage == message){ // Mensagem voltou pela segunda vez com erro, mensagem descartada
+								errorMessage = null;
+							 	System.out.println("Não foi possivel enviar a mensagem " + message.toString());
+							 } else {
+								System.out.println("Não foi possivel enviar a mensagem, sera enviada mais uma vez assim que o token voltar. "
+										+ message.toString());
+								message.setErrorControl(NAO_COPIADO);
+								errorMessage = message;
+							 }
+							 sendToken();
 						}
-						sendToken();
 					}
 				}
 
@@ -102,13 +117,14 @@ public class Server {
 	}
 
 	private void sendToken() {
+		configuration.setHasToken(false);
 		sendMessageToClient("1234");
 	}
 
 	private void printMessage(Message message) {
 		System.out.println("Source " + message.getNicknameSource());
-		System.out.println(message);
-		// TODO: Fazer o probalidade de ocorrer um erro
+		System.out.println("Print "+ message);
+
 		if (message.getDataType() == 'A') {
 			//TODO: salvar o conteudo em um arquivo
 			System.out.println("Salvando em um arquivo");
@@ -117,7 +133,12 @@ public class Server {
 
 	private void sendMessage() {
 		if (configuration.isHasToken() && !messages.isEmpty()) {
-			final Message remove = messages.remove(0);
+			final Message remove;
+			if (errorMessage != null) {
+				remove = errorMessage;
+			} else {
+				remove = messages.remove(0);
+			}
 			sendMessageToClient(remove.toString());
 		}
 	}
@@ -134,8 +155,11 @@ public class Server {
 						sendData.length,
 						address,
 						configuration.getPortDestiny());
-
+				System.out.println("Enviando " + configuration.getNickname());
+				Thread.sleep((long) (1000 * configuration.getTokenTime()));
 				clientSocket.send(sendPacket);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
 			}
 		} catch (SocketException e) {
 			e.printStackTrace();
